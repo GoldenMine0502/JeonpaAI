cf0c8ceae32ce90b133a854ac81dca12e4707100
GoldenMine0502 <ehe123@naver.com>
2023-10-22 03:32:12 +0900

    메인 파이프라인 구현

--- src/main.py
+++ src/main.py
@@ -1,7 +1,76 @@
-from configs import Config
+import os
+import math
+import torch
+import torch.nn as nn
 from model import Model
+from pathlib import Path
+from configs import Config
+from dataloader import create_dataloader
 
-config_path = '../config.yaml'
+# device
+device = torch.device('cpu')
+
+# config
+root_dir = Path(os.getcwd()).absolute()
+print(f"root directory: {root_dir}")
+
+config_path = f'{root_dir}/config.yaml'
 config = Config(config_path)
 
+# datasets
+trainloader = create_dataloader(config, True, root_dir)
+
+# model
 model = Model(config)
+model.to(device)
+
+# routes
+chkpt_path = f'{root_dir}/{config.log.chkpt_dir}'
+
+# train
+with open(config_path, 'r') as f:
+    hp_str = ''.join(f.readlines())
+
+optimizer = torch.optim.Adam(model.parameters(), lr=config.train.adam)
+
+step = 0
+# try:
+criterion = nn.MSELoss()
+while True:
+    model.train()
+    for train_seq, train_pred in trainloader:
+        result = model(train_seq)
+        loss = criterion(result, train_pred)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        step += 1
+        loss = loss.item()
+        if loss > 1e8 or math.isnan(loss):
+            print("Loss exploded to %.02f at step %d!" % (loss, step))
+            raise Exception("Loss exploded")
+
+        # write loss to tensorboard
+        if step % config.train.summary_interval == 0:
+            # writer.log_training(loss, step)
+            print("Wrote summary at step %d, loss: %f" % (step, loss))
+        # 1. save checkpoint file to resume training
+        # 2. evaluate and save sample to tensorboard
+        if step % config.train.checkpoint_interval == 0:
+            save_path = os.path.join(chkpt_path, 'chkpt_%d.pt' % step)
+            torch.save({
+                'model': model.state_dict(),
+                'optimizer': optimizer.state_dict(),
+                'step': step,
+                'hp_str': hp_str,
+            }, save_path)
+            print("Saved checkpoint to: %s" % save_path)
+            # validate(hp, audio, model, testloader, writer, step)
+
+        if step == 1000:
+            raise Exception(f"step {step}")
+
+# except Exception as e:
+#     print("Exiting due to exception: %s" % e)
+#     traceback.print_exc()
