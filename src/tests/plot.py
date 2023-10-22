27e5b593a2385a1294cc3b822630509d72995bb8
GoldenMine0502 <ehe123@naver.com>
2023-10-22 09:07:25 +0900

    plot test

--- src/tests/plot.py
+++ src/tests/plot.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from configs import Config
+from scipy.interpolate import interp1d
 # from scipy.interpolate import make_interp_spline, BSpline
 
 root_dir = Path(os.getcwd()).parent.absolute()
@@ -15,10 +16,19 @@ config = Config(config_path)
 datalist = [(f'{root_dir}/{config.data.trainset}', 'r'), ]
 
 for data, color in datalist:
-    data = pd.read_csv(data)['flux']
-    x = np.arange(len(data))
+    flux = pd.read_csv(data)['flux']
+    x = np.arange(len(flux))
 
-    plt.plot(x, data, color=color, linestyle='-', linewidth=1.5)
+    mean_flux = np.nanmean(flux)
+    flux[np.isnan(flux)] = mean_flux
+
+    # poly_interpolator = interp1d(np.arange(len(flux)), flux, kind='cubic')
+    # # 결측값 보간
+    # flux = poly_interpolator(np.arange(len(flux)))
+
+    print(flux)
+
+    plt.plot(x, flux, color=color, linestyle='-', linewidth=1.5)
 
 plt.show()
 
